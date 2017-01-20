e2b0993a6a87f472c37bef93ffcdfb52acb925a9
Swan Gone <greathairrecords@gmail.com>
2017-01-20 01:34:17 +0000

    Added some stuff

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to 'greathairrecords@gmail.com'
+  default to: 'greathairrecords@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
